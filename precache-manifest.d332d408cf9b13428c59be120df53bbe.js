cda43f34f001aa2e471b034e9ded84686cf937f2
hanhuayun <2256988633@qq.com>
2020-10-02 20:14:28 +0800

    ok

---
+++ precache-manifest.d332d408cf9b13428c59be120df53bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc0f37cc1dbbf79977656b122b2cc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef8e13f3138b6691e77",
+    "url": "/static/js/main.a5d72ceb.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
